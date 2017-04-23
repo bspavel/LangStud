68485df9b4e7d16f68d9bba010e05d2743f5de81
bspavel <bspavel@i.ua>
2017-04-23 20:22:43 +0300

    Changed few pages

--- assets/AngularAsset.php
+++ assets/AngularAsset.php
@@ -9,9 +9,9 @@ class AngularAsset extends AssetBundle
     public $sourcePath = '@bower';
     public $js = [
         'angular/angular.js',
-        'angular-animate/angular-animate.min.js',
-        'angular-bootstrap/ui-bootstrap.min.js',
-        'angular-bootstrap/ui-bootstrap-tpls.min.js'
+        //'angular-animate/angular-animate.min.js',
+        'bootstrap/ui-bootstrap.min.js',
+        'bootstrap/ui-bootstrap-tpls.min.js'
     ];
 
     public $jsOptions = [
